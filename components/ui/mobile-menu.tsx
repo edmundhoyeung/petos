e53294339580a7d18150dfe3ee37f29c8692cd05
edmundhoyeung <edmund@petionship.com>
2023-11-20 17:21:00 +0800

    update mobile menu

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -67,7 +67,7 @@ export default function MobileMenu() {
               rel="noopener noreferrer"
               className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
             >
-              Sign up
+              Book a call
             </Link>
           </li>
         </ul>
