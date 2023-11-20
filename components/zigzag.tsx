4c274f04cdcf1a3cb634c5a069b8b8f59396ec16
edmundhoyeung <edmund@petionship.com>
2023-11-20 18:46:24 +0800

    update 4th item

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import FeatImage01 from '@/public/images/screenshot_inventory.png'
 import FeatImage02 from '@/public/images/screenshot_customer.png'
 import FeatImage03 from '@/public/images/screenshot_service.png'
+import FeatImage04 from '@/public/images/screenshot_analytics.png'
 
 export default function Zigzag() {
   return (
@@ -132,32 +133,32 @@ export default function Zigzag() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
-                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width={540} height={405} alt="Features 02" />
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage04} width={540} height={405} alt="Features 02" />
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Stronger Customer Relationships</div>
-                  <h3 className="h3 mb-3">Know your customers and pets deeper</h3>
-                  <p className="text-xl text-gray-400 mb-4">Establish comprehensive customer and pet profiles to understand deeper about their purchasing preferences and behaviors for a more consumer-oriented CRM.</p>
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Smarter Performance Analytics</div>
+                  <h3 className="h3 mb-3">Track your pet shop performance smarter</h3>
+                  <p className="text-xl text-gray-400 mb-4">Visualize pet shop performance and store operations with actionable dashboards, financial & managerial reports, and data insights.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Capture important demographic info from your customers and pets</span>
+                      <span>View your sales data with a high level of granularity</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Create tiered loyalty programs to boost sales from your customers</span>
+                      <span>Analyze your PnL by key metrics without complex calculations</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Maximize customer lifetime value with personalized service plans</span>
+                      <span>Dig into your entire operation and supply chain with usable reports</span>
                     </li>
                   </ul>
                 </div>
