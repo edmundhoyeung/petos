1679c2d380a6f1beedb4b0e65506f0e4c36b2670
edmundhoyeung <edmund@petionship.com>
2023-11-20 18:06:31 +0800

    update custome feature

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 import FeatImage01 from '@/public/images/screenshot_inventory.png'
-import FeatImage02 from '@/public/images/features-03-image-02.png'
+import FeatImage02 from '@/public/images/screenshot_customer.png'
 import FeatImage03 from '@/public/images/features-03-image-03.png'
 
 export default function Zigzag() {
@@ -14,7 +14,7 @@ export default function Zigzag() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Run your pet business smarter</div>
             <h1 className="h2 mb-4">One product, multiple solutions</h1>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
+            <p className="text-xl text-gray-400">Running your pet business has never been easier with PetOS. You can focus on serving your furry customers better and we take care of everything else.</p>
           </div>
 
           {/* Items */}
@@ -65,27 +65,27 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">More speed. Less spend</div>
-                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
-                  <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Stronger Customer Relationships</div>
+                  <h3 className="h3 mb-3">Know your customers and pets deeper</h3>
+                  <p className="text-xl text-gray-400 mb-4">Establish comprehensive customer and pet profiles to understand deeper about their purchasing preferences and behaviors for a more consumer-oriented CRM.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Duis aute irure dolor in reprehenderit</span>
+                      <span>Capture important demographic info from your customers and pets</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Excepteur sint occaecat</span>
+                      <span>Create tiered loyalty programs to boost sales from your customers</span>
                     </li>
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Amet consectetur adipiscing elit</span>
+                      <span>Maximize customer lifetime value with personalized service plans</span>
                     </li>
                   </ul>
                 </div>
