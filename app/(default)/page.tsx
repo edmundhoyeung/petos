169beb00d9172266e36240df803b6570ba66aead
edmundhoyeung <edmund@petionship.com>
2023-11-20 18:20:30 +0800

    update meta

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'PetOS - All-in-one pet business management system powered by big data and AI',
+  description: 'Unlock the full potential of your pet business with PetOS. Our all-in-one pet business management system empowers you to efficiently manage inventory, analyze customer data, streamline store operations, and boost sales performance. Experience the convenience of a comprehensive software solution designed specifically for pet businesses. Try PetOS today and take your pet business to new heights.',
 }
 
 import Hero from '@/components/hero'
